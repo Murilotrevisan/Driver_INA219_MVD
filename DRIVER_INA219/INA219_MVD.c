2b209c0368b8e6a7faeef955fb4cc12643da20ce
Murilo_Trevisan <murilopasini@usp.br>
2021-09-14 10:15:54 -0300

    Alteração do tipo para complemento de 2

--- DRIVER_INA219/INA219_MVD.c
+++ DRIVER_INA219/INA219_MVD.c
@@ -32,34 +32,32 @@ error_t INA219_measure(INA219_t ina, INA219_values_t *medida) {
 
 		//Shunt Voltage
 		//Leitura do valor i2c no registrador do shunt
-		result16_t raw_s = i2c_read16(ina.device, INA_SHUNT_VOLT);
+		int16_t raw_s = i2c_read16(ina.device, INA_SHUNT_VOLT);
 
-		//Analise de erro
-		if (raw_s.hasError) {
-			return 1;
-		}
+
 		//conversao do valor lido para tensão no Shunt
 		medida -> Shunt_Voltage = ((float) raw_s.value) * INA_Shunt_Multiply; //Configurar valores
 
+
 		//Bus Voltage
 		//Leitura do valor i2c do registrador Bus
-		result16_t raw_b = i2c_read16(ina.device, INA_BUS_VOLT);
+		int16_t raw_b = i2c_read16(ina.device, INA_BUS_VOLT);
 
-		//Analise de erro
-		if (raw.hasError) {
-			return 1;
-		}
 
-		uint16_t Val_Shift = raw_b.value >> 3;
+		int16_t Val_Shift = raw_b.value >> 3;
 		//conversão do valor lida para tensão no Bus
 		medida -> Bus_Voltage = ((float) Val_Shift) * INA_Bus_Multiply; //configurar valores
+
 
 		//Current
 		//Calculo a partir da tensão no Shunt e do valor do resistor do datasheet
 		result16_t raw_c = (Shunt_Voltage)/(INA_Resistor_Shunt);
 
-		medida -> Current_Shunt = ((float) raw_c); //configurar valores
+		if (raw_c.hasError){
+			return 1;
+		}
 
+		medida -> Current_Shunt = ((float) raw_c.value); //configurar valores
 
 }
 
